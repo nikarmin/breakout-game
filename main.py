f3d135ef6bfe90c5c4d37322294f8da9105a419a
nikarmin <nikafworks@gmail.com>
2022-11-24 16:36:07 -0300

    menu

---
+++ main.py
@@ -0,0 +1,148 @@
+import pygame as pg
+
+from pygame.locals import (
+    K_UP, K_DOWN, K_LEFT, K_RIGHT,
+    K_ESCAPE, KEYDOWN, QUIT, K_SPACE
+)
+
+import barrinha # importando o arquivo da barrinha
+
+pg.init()
+pg.mixer.init()
+
+# definindo largura e altura da tela
+X = 800
+Y = 700
+size = (X, Y)
+
+# definindo cores
+AZUL = (0, 97, 148)
+PRETO = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (212, 210, 212)
+RED = (162, 8, 0)
+ORANGE = (183, 119, 0)
+GREEN = (0, 127, 33)
+YELLOW = (197, 199, 37)
+
+lista_sprites = pg.sprite.Group()
+
+# define a janela
+screen = pg.display.set_mode(size)
+pg.display.set_caption("Breakout Game")
+
+# definindo fonte
+fonte = pg.font.Font('assets/font_bit.ttf', 30)
+
+# menu - sair
+txtSair = fonte.render('Sair', False, (255, 255, 255), (0, 0, 0))
+pSair = (screen.get_width() / 2 - txtSair.get_width() / 2, (screen.get_height() * 2) / 3 - txtSair.get_height() / 2)
+btnSair = txtSair.get_rect(topleft=pSair)
+
+# menu - jogar
+txtJogar = fonte.render('Jogar', False, (255, 255, 255), (0, 0, 0))
+pJogar = (screen.get_width() / 2 - txtJogar.get_width() / 2, (screen.get_height() * 2) / 4 - txtJogar.get_height() / 2)
+btnJogar = txtJogar.get_rect(topleft=pJogar)
+
+# menu - logo
+
+fonte = pg.font.Font('assets/font_bit.ttf', 50)
+
+txtLogo = fonte.render('BreakOut', False, (255, 255, 255), (0, 0, 0))
+pLogo = screen.get_width() / 2 - txtLogo.get_width() / 2, screen.get_height() / 3  - txtLogo.get_height() / 2
+
+# testando a barrinha
+LARGURA_BARRINHA = 108
+ALTURA_BARRINHA  = 20
+barra = barrinha.Barrinha(AZUL)
+barra.rect.x = X // 2 - LARGURA_BARRINHA // 2
+barra.rect.y = Y - 65
+
+lista_sprites.add(barra)
+
+clock = pg.time.Clock()
+FPS = 60
+
+# CLASSE DOS TIJOLOS
+
+class Brick(pg.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pg.Surface([width, height])
+        pg.draw.rect(self.image, color, [0, 0, width, height])
+        self.rect = self.image.get_rect()
+
+# CLASSE DA BOLA
+
+
+# altura e largura dos tijolinhos
+brick_width = 55
+brick_height = 16
+x_gap = 7
+y_gap = 5
+wall_width = 16
+
+# método da main do jogo
+
+def main():
+    running = True
+    clock.tick(FPS)
+    while running:
+        screen.fill(PRETO)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        if btnJogar.collidepoint(event.pos):
+                            jogar()
+                            #print('Botão "jogar" apertado!')
+                            # colocar evento para iniciar a partida
+                        if btnSair.collidepoint(event.pos):
+                            running = False
+        
+        #lista_sprites.update()
+                            
+        screen.blit(txtLogo, pLogo)
+        screen.blit(txtJogar, pJogar)
+        screen.blit(txtSair, pSair)
+
+        #pg.draw.line(screen, GREY, [0, 19], [X, 19], 40)
+
+        #lista_sprites.draw(screen)
+
+        pg.display.update()
+        pg.display.flip()
+    
+    # finaliza o pygame
+    pg.quit()
+
+# método 'jogar'
+    
+def jogar():
+    running = True
+    clock.tick(FPS)
+    while running:
+        screen.fill(PRETO)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+        
+        lista_sprites.update()
+        pg.draw.line(screen, GREY, [0, 19], [X, 19], 40)
+
+        lista_sprites.draw(screen)
+
+        pg.display.update()
+        pg.display.flip()
+    
+    # finaliza o pygame
+    pg.quit()
+
+main()
