59a3f522b4eafe1e0bf28d112b75c08558d93c06
NICOLI FERREIRA <u21689@academico.cotuca.unicamp.br>
2022-11-25 11:13:31 -0300

    bolinha 🎱

--- bolinha.py
+++ bolinha.py
@@ -1,12 +1,18 @@
 import pygame as pg
+from random import randint
+
 
 class Bolinha(pg.sprite.Sprite):
-    def __init__(self, cor, larg, alt) -> None:
+    def __init__(self, cor, larg, alt):
         super().__init__()
-        self.image = pg.Surface([alt, larg])
-        pg.draw.rect(self.image, cor, [0,0, larg, alt])
+        self.largura = larg
+        self.altura = alt
+        self.image = pg.Surface([self.largura, self.altura])
+        self.image.fill(cor)
+        self.image.set_colorkey(cor)
+        pg.draw.rect(self.image, cor, [0, 0, self.largura, self.altura])
+        self.velocity = [randint(4, 8), randint(-8, 8)]
         self.rect = self.image.get_rect()
-        self.velocity = [4, 4]
 
     def update(self):
         self.rect.x += self.velocity[0]
@@ -14,4 +20,4 @@ class Bolinha(pg.sprite.Sprite):
 
     def balancar(self):
         self.velocity[0] = self.velocity[0]
-        self.velocity[1] = -self.velocity[1]
+        self.velocity[1] = -self.velocity[-8, 8]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
-import bolinha as bola
+import bolinha
+from random import randint
 
 # pegando as teclas
 from pygame.locals import (
@@ -7,10 +8,10 @@ from pygame.locals import (
     K_ESCAPE, KEYDOWN, QUIT, K_SPACE
 )
 
-import barrinha # importando o arquivo da barrinha
+import barrinha  # importando o arquivo da barrinha
 
 pg.init()       # iniciando o jogo
-pg.mixer.init() # iniciando o mixer (para sons)
+pg.mixer.init()  # iniciando o mixer (para sons)
 
 # definindo largura e altura da tela
 X = 800
@@ -38,31 +39,65 @@ fonte = pg.font.Font('assets/bit.ttf', 30)
 
 # menu - sair
 txtSair = fonte.render('Sair', False, (255, 255, 255), (0, 0, 0))
-pSair = (screen.get_width() / 2 - txtSair.get_width() / 2, (screen.get_height() * 2) / 3 - txtSair.get_height() / 2)
+pSair = (screen.get_width() / 2 - txtSair.get_width() / 2,
+         (screen.get_height() * 2) / 3 - txtSair.get_height() / 2)
 btnSair = txtSair.get_rect(topleft=pSair)
 
 # menu - jogar
 txtJogar = fonte.render('Jogar', False, (255, 255, 255), (0, 0, 0))
-pJogar = (screen.get_width() / 2 - txtJogar.get_width() / 2, (screen.get_height() * 2) / 4 - txtJogar.get_height() / 2)
+pJogar = (screen.get_width() / 2 - txtJogar.get_width() / 2,
+          (screen.get_height() * 2) / 4 - txtJogar.get_height() / 2)
 btnJogar = txtJogar.get_rect(topleft=pJogar)
 
 # menu - logo
 fonte = pg.font.Font('assets/bit.ttf', 50)
 
 txtLogo = fonte.render('BreakOut', False, (255, 255, 255), (0, 0, 0))
-pLogo = screen.get_width() / 2 - txtLogo.get_width() / 2, screen.get_height() / 3  - txtLogo.get_height() / 2
+pLogo = screen.get_width() / 2 - txtLogo.get_width() / \
+    2, screen.get_height() / 3 - txtLogo.get_height() / 2
 
 # testando a barrinha
 LARGURA_BARRINHA = 108
-ALTURA_BARRINHA  = 20
+ALTURA_BARRINHA = 20
 barra = barrinha.Barrinha(AZUL, LARGURA_BARRINHA, ALTURA_BARRINHA)
 barra.rect.x = X // 2 - LARGURA_BARRINHA // 2
 barra.rect.y = Y - 65
+
 
 clock = pg.time.Clock()
 FPS = 60
 
 # CLASSE DOS TIJOLOS
+
+velocity = 1
+
+# pg.draw.line(screen, CINZA, [0, 19], [X, 19], 40)
+
+
+class Ball(pg.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pg.Surface([width, height])
+        pg.draw.rect(self.image, color, [
+                     0, 0, width, height], border_radius=100)
+        self.rect = self.image.get_rect()
+        self.velocity = [1, 1]
+
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+
+    def bounce(self):
+        self.velocity[0] = self.velocity[0]
+        self.velocity[1] = -self.velocity[-8, 8]
+
+
+# definindo bolinha
+bolinha = Ball(VERMELHO, 20, 20)
+bolinha.rect.x = X // 2 - 5
+bolinha.rect.y = Y // 2 - 5
+
 
 class Brick(pg.sprite.Sprite):
 
@@ -80,15 +115,11 @@ x_gap = 7
 y_gap = 5
 wall_width = 16
 
-# definindo bolinha
-bolinha = bola.Bolinha((212, 210, 212), 10, 10)
-bolinha.rect.x = Y
-bolinha.rect.y = X
-    
 # adicionando nas sprites
 
 lista_sprites.add(barra)
 lista_sprites.add(bolinha)
+
 
 def main():
     running = True
@@ -96,36 +127,37 @@ def main():
     while running:
         screen.fill(PRETO)
 
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-            if event.type == pg.MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        if btnJogar.collidepoint(event.pos):
-                            jogar()
-                            running = False
-                            #print('Botão "jogar" apertado!')
-                            # colocar evento para iniciar a partida
-                        if btnSair.collidepoint(event.pos):
-                            running = False
-    
         screen.blit(txtLogo, pLogo)
         screen.blit(txtJogar, pJogar)
         screen.blit(txtSair, pSair)
 
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if btnJogar.collidepoint(event.pos):
+                        jogar()
+                        running = False
+                        #print('Botão "jogar" apertado!')
+                        # colocar evento para iniciar a partida
+                    if btnSair.collidepoint(event.pos):
+                        running = False
+
         pg.display.update()
         pg.display.flip()
-    
+
     # finaliza o pygame
     pg.quit()
 
 # método 'jogar'
+
+
 def jogar():
     running = True
     clock.tick(FPS)
     while running:
         screen.fill(PRETO)
-
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
@@ -133,31 +165,47 @@ def jogar():
                 if event.key == K_ESCAPE:
                     running = False
 
+        lista_sprites.update()
 
         teclas = pg.key.get_pressed()
         if teclas[pg.K_LEFT]:
             barra.irParaEsquerda(1)
         if teclas[pg.K_RIGHT]:
             barra.irParaDireita(1, X)
-        
-        lista_sprites.update()
+
+        if bolinha.rect.x >= 790:
+            bolinha.velocity[0] = -bolinha.velocity[0]
+        if bolinha.rect.x <= 0:
+            bolinha.velocity[0] = -bolinha.velocity[0]
+        if bolinha.rect.y > 590:
+            bolinha.velocity[1] = -bolinha.velocity[1]
+        if bolinha.rect.y < 40:
+            bolinha.velocity[1] = -bolinha.velocity[1]
+
+        if pg.sprite.collide_mask(bolinha, barra):
+            bolinha.rect.x -= bolinha.velocity[0]
+            bolinha.rect.y -= bolinha.velocity[1]
+            bolinha.bounce()
+
         pg.draw.line(screen, CINZA, [0, 19], [X, 19], 40)
 
         lista_sprites.draw(screen)
 
         pg.display.update()
         pg.display.flip()
-    
+
     # finaliza o pygame
 
 # método 'perdeu'
-    
+
+
 def perdeu():
     # arrumar
     screen.fill(PRETO)
     fonte = pg.font.Font('assets/minecraft.ttf', 100)
     txtPerdeu = fonte.render('Voce perdeu!', False, (255, 0, 0), (0, 0, 0))
-    pPerdeu = (screen.get_width() / 2 - txtPerdeu.get_width() / 2, (screen.get_height() * 2) / 4 - txtPerdeu.get_height() / 2)
+    pPerdeu = (screen.get_width() / 2 - txtPerdeu.get_width() / 2,
+               (screen.get_height() * 2) / 4 - txtPerdeu.get_height() / 2)
 
     # usuário escolher se quer retornar ou sair
 
@@ -167,6 +215,7 @@ def perdeu():
 
     pg.display.update()
     pg.display.flip()
+
 
 if __name__ == '__main__':
     main()
